800e67b5bc0715e99f7354f7ce41c79c1e8f8d2d
Gleb Vinnychenko <65500261+hv275@users.noreply.github.com>
2022-05-26 22:15:00 +0100

    WIP commit for parser

--- parse.py
+++ parse.py
@@ -1,3 +1,11 @@
+#########
+# these are temp imports for development
+from unittest.mock import Mock
+from scanner import Symbol
+
+#########
+
+
 """Parse the definition file and build the logic network.
 
 Used in the Logic Simulator project to analyse the syntactic and semantic
@@ -35,10 +43,78 @@ class Parser:
 
     def __init__(self, names, devices, network, monitors, scanner):
         """Initialise constants."""
+        self.__names = names
+        self.scanner = scanner
+        self.success = 1
 
+    # note, this is yet to be tested in combination with the scanner, mocking is used for symbols atm
     def parse_network(self):
         """Parse the circuit definition file."""
         # For now just return True, so that userint and gui can run in the
         # skeleton code. When complete, should return False when there are
         # errors in the circuit definition file.
-        return True
+
+        if (
+            self.symbol.type == self.scanner.KEYWORDS
+            and self.symbol.id == self.scanner.DEVICES_ID
+        ):
+            self.symbol = self.scanner.getsymbol()
+            self.device()
+            while self.symbol.type != self.scanner.ENDBRACK:
+                self.symbol = self.scanner.getsymbol()
+                self.device()
+            self.symbol = self.scanner.getsymbol()
+
+        elif (
+            self.symbol.type == self.scanner.KEYWORDS
+            and self.symbol.id == self.scanner.CONNECT_ID
+        ):
+            self.symbol = self.scanner.getsymbol()
+            self.make_connection()
+            while self.symbol.type != self.scanner.ENDBRACK:
+                self.symbol = self.scanner.getsymbol()
+                self.scanner.make_connection()
+            self.symbol = self.scanner.getsymbol()
+
+        elif (
+            self.symbol.type == self.scanner.KEYWORDS
+            and self.symbol.id == self.scanner.MONITOR_ID
+        ):
+            self.symbol = self.scanner.getsymbol()
+            self.make_monitor()
+            while self.symbol.type != self.scanner.ENDBRACK:
+                self.symbol = self.scanner.getsymbol()
+                self.make_monitor()
+            self.symbol - self.scanner.getsymbol()
+
+        elif self.symbol.type == self.scanner.ENDBRACK:
+            self.symbol = self.scanner.getsymbol()
+
+        elif (
+            self.symbol.type == self.scanner.KEYWORDS
+            and self.symbol.id == self.scanner.END
+        ):
+            return 1 if self.success else 0
+        else:
+            self.error()
+
+        def make_monitor(self):
+            """
+            Fucntion to parse the monitor line as per the EBNF spec
+            """
+            self.device_name()
+            if self.symbol.type == self.__scanner.DOT:
+                self.output_pin()
+            self.symbol = self.scanner.getsymbol()
+            if self.symbol.type == self.scanner.SEMICOLON:
+                # TODO decide how ot do return properly
+                self.symbol = self.scanner.getsymbol()
+                return 1
+            else:
+                # TODO sort out proper error handling and display
+                self.error()
+
+        def create_conn():
+            """
+            Function to parse connection creation as per EBNF spec
+            """
