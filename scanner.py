723c778a136a69b7280f183a875ba550f4d803e4
Nikodem Czarlinski <nczarlinski@gmail.com>
2022-05-29 09:50:56 +0100

    Comment Code
    - Commented code ready for start of integration work with parser
    - Todo lists included
    - PEP 8/257 compliant

--- scanner.py
+++ scanner.py
@@ -13,7 +13,6 @@ from names import Names
 
 
 class Symbol:
-
     """Encapsulate a symbol and store its properties.
 
     Parameters
@@ -32,7 +31,6 @@ class Symbol:
 
 
 class Scanner:
-
     """Read circuit definition file and translate the characters into symbols.
 
     Once supplied with the path to a valid definition file, the scanner
@@ -139,7 +137,7 @@ class Scanner:
             self.RIGHT_CURLY_BRACKET_ID] = self.names.lookup(
                                                 self.curly_bracket_list)
 
-        self.current_character = "*"
+        self.current_character = "*" # TODO: Remove special current char
 
         # Opens the definition file
         self.file = open(path)
@@ -157,19 +155,13 @@ class Scanner:
         while self.current_character.isspace():
             self.advance()
 
-    def get_name(self):
-        """get_name is similar to get_next_name in the preliminary exercises,
-        except that it now assumes the current character is a letter, returns
-        only the name string and places the next nonalphanumeric character
-        in current_character"""
-
-    def get_right_arrow(self):
-        """Assumes that the current character is an equals"""
-
     def get_device_name(self):
-        """Assumes that the initial character is a lower case character. Then
-        finds the whole device name. Device name ended when a nonalphanumberic
-        character is found."""
+        """Extract the device name in the following seq of characters.
+        
+        Assumes that the initial character is a lower case character. Then
+        finds the whole device name. Device name ended when a nonalphanumeric
+        character is found.
+        """
         char = "a"
         name_string = ""
 
@@ -183,9 +175,12 @@ class Scanner:
                 return (name_string, char)
 
     def get_capital_name(self):
-        """Assumes that the initial character is a upper case character. Then
+        """Extract name in sequence of capital characters.
+        
+        Assumes that the initial character is a upper case character. Then
         finds the whole uppercase word. Word ended when a non uppercase
-        character is found."""
+        character is found.
+        """
         char = "A"
         name_string = ""
 
@@ -199,8 +194,11 @@ class Scanner:
                 return (name_string, char)
 
     def get_number(self):
-        """Assumes current character is a number. Finds the whole number.
-        Termination occurs when non numeric character is found."""
+        """Extract number in the the next sequence of characters.
+        
+        Assumes current character is a number. Finds the whole number.
+        Termination occurs when non numeric character is found.
+        """
         char = "1"
         number_string = ""
 
@@ -215,7 +213,7 @@ class Scanner:
 
     def get_symbol(self):
         """Translate the next sequence of characters into a symbol."""
-
+        # TODO: Lower the if statement number by using OOP and for loops
         symbol = Symbol()
         if self.current_character == "*":
             self.skip_spaces()  # current character now not whitespace
@@ -315,6 +313,7 @@ class Scanner:
         return symbol
 
 
+# Run file and simple test
 names_instance = Names()
 
 path_definition = "definitions/circuit.def"
