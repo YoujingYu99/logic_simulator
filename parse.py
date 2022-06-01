b4020df67205a5b2fdf9894326f6ff5a313ec533
Nikodem Czarlinski <nczarlinski@gmail.com>
2022-06-01 22:56:18 +0100

    Fix bug in gate definition creating too many gates

--- parse.py
+++ parse.py
@@ -200,7 +200,7 @@ class Parser:
             else:
                 self.error("OUTPUT_PIN_EXPECTED")
 
-        self.monitors.make_monitor(device_id, output_id, cycles_completed=0)
+        self.monitors.make_monitor(device_id, output_id, cycles_completed=10)
 
         if self.symbol.type == self.scanner.SEMICOLON:
             pass
@@ -309,9 +309,17 @@ class Parser:
             self.symbol = self.scanner.get_symbol()
             # FIXME type of symbol ID behaves a bit weirdly
             # not sure if this is intentional
-            self.input_number()
-            device_property = self.symbol.id
-            self.symbol = self.scanner.get_symbol()
+            # self.input_number()
+
+            if self.symbol.type == self.scanner.NUMBER and (
+            int(self.symbol.id) in range(1, 17)
+            ):
+                device_property = self.symbol.id
+                self.symbol = self.scanner.get_symbol()
+            else:
+                # error of invalid input
+                self.error("INVALID_INPUT_INITIALISATION")
+
             if self.symbol.id != self.scanner.RIGHT_BRACKET_ID:
                 self.error("RIGHT_BRACKET_EXPECTED")
             else:
@@ -327,7 +335,6 @@ class Parser:
             while self.symbol.type == self.scanner.COMMA:
                 self.device_name()
                 device_id = self.symbol.id
-                self.symbol = self.scanner.get_symbol()
                 if not (
                     self.symbol.type == self.scanner.BRACKET
                     and self.symbol.id == self.scanner.LEFT_BRACKET_ID
@@ -335,8 +342,15 @@ class Parser:
                     self.error("LEFT_BRACKET_EXPECTED")
                 else:
                     self.symbol = self.scanner.get_symbol()
-                    self.input_number()
-                    device_property = self.symbol.id
+                    # self.input_number()
+                    if self.symbol.type == self.scanner.NUMBER and (
+                    int(self.symbol.id) in range(1, 17)
+                    ):
+                        device_property = self.symbol.id
+                        self.symbol = self.scanner.get_symbol()
+                    else:
+                        # error of invalid input
+                        self.error("INVALID_INPUT_INITIALISATION")
                     if not (
                         self.symbol.type == self.scanner.BRACKET
                         and self.symbol.id == self.scanner.RIGHT_BRACKET_ID
@@ -733,12 +747,14 @@ class Parser:
 # parser_logger = logging.getLogger("parser")
 # logging.basicConfig(level=logging.DEBUG)
 #
+
 # names_instance = Names()
 # scanner_instance = Scanner(path_definition, names_instance, scanner_logger)
 # device_instance = Devices(names_instance)
 # network_instance = Network(names_instance, device_instance)
 # monitor_instance = Monitors(names_instance, device_instance, network_instance)
 #
+
 # parser_1 = Parser(
 #     names_instance,
 #     device_instance,
@@ -748,6 +764,7 @@ class Parser:
 #     parser_logger,
 # )
 #
+
 # a = parser_1.parse_network()
 # print('--Check all devices have been created')
 # print(parser_1.devices.find_devices())
@@ -766,3 +783,60 @@ class Parser:
 # print(monitored_signal_list)
 #
 # parser_1.monitors.display_signals()
+# print(parser_1.devices.get_device(42).outputs) # DTYPE 
+# print(parser_1.devices.get_device(46).inputs)
+
+
+# print('--Check all network inputs are satisfied')
+# print(parser_1.network.check_network())
+
+
+# monitored_signal_list, non_monitored_signal_list = parser_1.monitors.get_signal_names()
+
+# print('--List monitor points')
+# print(monitored_signal_list)
+
+# parser_1.monitors.display_signals()
+
+
+# For circuit 2
+path_definition = "definitions/circuit2.def"
+scanner_logger = logging.getLogger("scanner")
+parser_logger = logging.getLogger("parser")
+logging.basicConfig(level=logging.DEBUG)
+
+names_instance = Names()
+scanner_instance = Scanner(path_definition, names_instance, scanner_logger)
+device_instance = Devices(names_instance)
+network_instance = Network(names_instance, device_instance)
+monitor_instance = Monitors(names_instance, device_instance, network_instance)
+
+parser_1 = Parser(
+    names_instance,
+    device_instance,
+    network_instance,
+    monitor_instance,
+    scanner_instance,
+    parser_logger,
+)
+
+a = parser_1.parse_network()
+print('--Confirm that and gate has been created')
+print(parser_1.devices.find_devices(parser_1.scanner.AND_ID)) 
+print('--Confirm that switches have been created')
+print(parser_1.devices.find_devices(parser_1.scanner.SWITCH_ID)) 
+
+
+print('--ANDg inputs')
+print(parser_1.devices.get_device(42).inputs) 
+print(parser_1.devices.get_device(42).outputs)
+
+print('--Check all network inputs are satisfied')
+print(parser_1.network.check_network())
+
+monitored_signal_list, non_monitored_signal_list = parser_1.monitors.get_signal_names()
+
+print('--List monitor points')
+print(monitored_signal_list)
+
+parser_1.monitors.display_signals()
