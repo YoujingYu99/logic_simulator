0ef4184fd8095b74725d843ecfdc90684b1b3e49
Gleb Vinnychenko <65500261+hv275@users.noreply.github.com>
2022-06-02 14:06:09 +0100

    Tests added up to dtype_devices, WIP

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -70,8 +70,15 @@ END"""
 
 
 @patch("parse.Parser.error")
-@pytest.mark.parametrize("text,error", [("dtype", "SEMICOLON_EXPECTED")])
-def test_make_monitor(error_mock, text, error):
+@pytest.mark.parametrize(
+    "text,error",
+    [
+        ("dtype", "SEMICOLON_EXPECTED"),
+        ("dtype.;", "OUTPUT_PIN_EXPECTED"),
+        (";", "DEVICE_NAME_EXPECTED"),
+    ],
+)
+def test_make_monitor_errors(error_mock, text, error):
     """
     Function to test make_monitor method of parser
     """
@@ -79,3 +86,124 @@ def test_make_monitor(error_mock, text, error):
     parser.symbol = parser.scanner.get_symbol()
     parser.make_monitor()
     error_mock.assert_called_with(error)
+
+
+@patch("parse.Parser.error")
+def test_make_monitor_basic(error_mock):
+    """
+    Function to tetst make_monitor works by itself
+    """
+    text = "dtype.Q;"
+    parser = return_parser(text)
+    parser.symbol = parser.scanner.get_symbol()
+    parser.make_monitor()
+
+    assert error_mock.not_called
+
+
+@patch("parse.Parser.error")
+@pytest.mark.parametrize(
+    "text,error",
+    [
+        ("sw1 => dtype.SET", "SEMICOLON_EXPECTED"),
+        ("dtype1. => dtype.SET;", "OUTPUT_PIN_EXPECTED"),
+        (" ;", "DEVICE_NAME_EXPECTED"),
+        ("sw1 => ;", "DEVICE_NAME_EXPECTED"),
+        ("sw1 dtype.SET;", "RIGHT_ARROW_EXPECTED"),
+        ("sw1 => dtype;", "INPUT_SPECIFICATION_EXPECTED"),
+        ("sw1 => dtype.FOO;", "NOT_VALID_INPUT"),
+    ],
+)
+def test_create_conn_errors(error_mock, text, error):
+    """
+    Function to test make_monitor method of parser
+    """
+    parser = return_parser(text)
+    parser.symbol = parser.scanner.get_symbol()
+    parser.create_conn()
+    # note, no assertion of being called once as error skipping is not functional
+    error_mock.assert_any_call(error)
+
+
+@patch("parse.Parser.error")
+@pytest.mark.parametrize(
+    "text",
+    [("sw1 => dtype.SET;"), ("dtype.Q => gate.I1;")],
+)
+def test_create_conn_clean(error_mock, text):
+    """
+    Function to test make_monitor method of parser
+    """
+    parser = return_parser(text)
+    parser.symbol = parser.scanner.get_symbol()
+    parser.create_conn()
+    # note, no assertion of being called once as error skipping is not functional
+    error_mock.assert_not_called()
+
+
+@patch("parse.Parser.error")
+def test_device_errors(error_mock):
+    """
+    Function to test error handling of device method
+    """
+    text = "FOO"
+    parser = return_parser(text)
+    parser.symbol = parser.scanner.get_symbol()
+    parser.device()
+    error_mock.assert_called_with("DEVICE_TYPE_NOT_DECLARED")
+
+
+@patch.multiple(
+    "parse.Parser",
+    clock_devices=DEFAULT,
+    switch_devices=DEFAULT,
+    dtype_devices=DEFAULT,
+    gate_devices=DEFAULT,
+    error=DEFAULT,
+)
+@pytest.mark.parametrize(
+    "text", ["CLOCK", "SWITCH", "DTYPE", "AND", "NAND", "OR", "NOR"]
+)
+def test_device_clean(text, **mocks):
+    parser = return_parser(text)
+    parser.symbol = parser.scanner.get_symbol()
+    parser.device()
+    parser.error.assert_not_called()
+
+
+@patch.multiple(
+    "parse.Parser",
+    input_number=DEFAULT,
+    error=DEFAULT,
+)
+@pytest.mark.parametrize(
+    "text,test_error",
+    [
+        ("and10);", "LEFT_BRACKET_EXPECTED"),
+        ("and(10;", "RIGHT_BRACKET_EXPECTED"),
+        ("and(10), and210);", "LEFT_BRACKET_EXPECTED"),
+        ("and(10), and2(10 ;", "RIGHT_BRACKET_EXPECTED"),
+        ("and(10)", "SEMICOLON_EXPECTED"),
+        ("and(10), and2(10)", "SEMICOLON_EXPECTED"),
+    ],
+)
+def test_gate_devices_errors(text, test_error, **mocks):
+    parser = return_parser(text)
+    parser.gate_devices(mock.Mock())
+    parser.error.assert_any_call(test_error)
+
+
+@patch("parse.Parser.error")
+@pytest.mark.parametrize("text,test_error", [("dtype", "SEMICOLON_EXPECTED")])
+def test_dtype_devices_errors(error_mock, text, test_error):
+    parser = return_parser(text)
+    parser.gate_devices(mock.Mock())
+    error_mock.assert_any_call(test_error)
+
+
+@patch("parse.Parser.error")
+def test_dtype_devices_clean(error_mock):
+
+    parser = return_parser("dtype;")
+    parser.dtype_devices(mock.Mock())
+    error_mock.assert_not_called()
